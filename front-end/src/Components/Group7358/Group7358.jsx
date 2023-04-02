a62049c36c60ab2d5bdd3b3685788f6943572181
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:30:20 -0500

    Create Group7358.jsx

---
+++ front-end/src/Components/Group7358/Group7358.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Group7358() {
+  return (
+    <div class="group7358">
+    <div class="rectangle2482">
+    </div>
+    <div class="rectangle2481">
+    </div>
+    </div>
+  )
+}
+
+export default Group7358;
