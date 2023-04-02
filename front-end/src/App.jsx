f72cd733ad4dd3105df98ffd0a3e63199fea67ab
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:23:59 -0500

    Create App.jsx

---
+++ front-end/src/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import CreateNewTour from './components/CreateNewTour/CreateNewTour'
+import Description from './components/Description/Description'
+import Details from './components/Details/Details'
+import Group7358 from './components/Group7358/Group7358'
+import InvestAndEarn from './components/InvestAndEarn/InvestAndEarn'
+import KeyFacts from './components/KeyFacts/KeyFacts'
+import Logo from './components/Logo/Logo'
+import Logout from './components/Logout/Logout'
+import LoremTop from './components/LoremTop/LoremTop'
+import NeedHelp from './components/NeedHelp/NeedHelp'
+import Notification from './components/Notification/Notification'
+import Profile from './components/Profile/Profile'
+import PropertyDetails from './components/PropertyDetails/PropertyDetails'
+import PropertyHeading from './components/PropertyHeading/PropertyHeading'
+import Rectangle2450 from './components/Rectangle2450/Rectangle2450'
+import Rectangle2478 from './components/Rectangle2478/Rectangle2478'
+import Rectangle2479 from './components/Rectangle2479/Rectangle2479'
+import Rooms from './components/Rooms/Rooms'
+import SearchNormal from './components/SearchNormal/SearchNormal'
+import SettingIcon from './components/SettingIcon/SettingIcon'
+import Shortcuts from './components/Shortcuts/Shortcuts'
+import SideAddList from './components/SideAddList/SideAddList'
+import SideBar from './components/Sidebar/SideBar'
+
+function App() {
+  return (
+    <div>
+    <SideBar />
+    <SideAddList />
+    <Shortcuts />
+    <PropertyDetails />
+    <PropertyHeading />
+    <Logo />
+    <CreateNewTour />
+    <Profile />
+    <Logout />
+    <SettingIcon /> 
+    <SearchNormal />
+    <Notification />
+    <Rectangle2478 />
+    <Rectangle2450 />
+    <Rectangle2479 />
+    <KeyFacts />
+    <Details />
+    <Rooms />
+    <Group7358 />
+    <LoremTop />
+    <Description />
+    <NeedHelp />
+    <InvestAndEarn />
+    </div>
+  )
+}
+
+export default App
