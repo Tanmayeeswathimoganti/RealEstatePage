643c707e2fc5868e524c369b8d04805c5d704004
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:29:31 -0500

    Create Description.jsx

---
+++ front-end/src/Components/Description/Description.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Description() {
+  return (
+    <div className='description'>
+    <div className='descriptionHeading'>
+    Description
+    </div>
+
+    <div className='descriptionContent'>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo risus a mattis semper. Integer placerat volutpat odio, at pulvinar tortor laoreet mattis. Proin aliquet semper tempus. Etiam auctor lacus leo <span>read more..</span>
+    </div>
+    </div>
+  )
+}
+
+export default Description;
