0b4636dbea4c4fe501da971f885d485d8c347bf2
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:31:24 -0500

    Create InvestAndEarn.jsx

---
+++ front-end/src/Components/InvestAndEarn/InvestAndEarn.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Invite from '../../assets/userCirlceAdd.svg';
+
+function InvestAndEarn() {
+  return (
+    <div className='investAndEarn'>
+    <div className='group7019'>
+    <div className='icon'>
+    <img src={Invite}/>
+    </div>
+    <div className='content'>
+    Invite & Earn
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default InvestAndEarn;
