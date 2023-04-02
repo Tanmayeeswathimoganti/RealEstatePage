029ee52d129e402ce7f5f7c8f6208de5ed74b5cb
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:36:14 -0500

    Create PropertyHeading.jsx

---
+++ front-end/src/Components/PropertyHeading/PropertyHeading.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ArrowLeft from "../../assets/arrow-left-s-line.svg";
+
+function PropertyHeading() {
+  return (
+    <div className='group7625'>
+    <div className='propertyArrow'>
+    <img src={ArrowLeft} className="arrowImage"/>
+    </div>
+    <div className='propertyHeading'>
+    2218 Thornridge Cir.. E5579076
+    </div>
+    </div>
+  )
+}
+
+export default PropertyHeading;
