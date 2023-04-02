cd77f5bddc9cc8100967d09db5e413595da71604
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:38:52 -0500

    Create Searchnormal.jsx

---
+++ front-end/src/Components/Searchnormal/Searchnormal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import searchNormal from "../../assets/search-normal.svg";
+
+function SearchNormal() {
+  return (
+    <div className='searchNormal'>
+    <img src={searchNormal} />
+    </div>
+  )
+}
+
+export default SearchNormal;
