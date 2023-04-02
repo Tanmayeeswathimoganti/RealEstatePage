5e4eec26208c998eed41093f2a853b0c053b13f1
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:39:50 -0500

    Create ShortCutList.jsx

---
+++ front-end/src/Components/Shortcuts/ShortCutList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import toggle from '../../assets/toggle.svg';
+
+function ShortCutList() {
+  return (
+    <div className='group7242'>
+      <div className="rectangle2452">
+      <div className='notify'>
+      Notify similar properties
+      </div>
+      <label class="switch">
+      <input type="checkbox" />
+      <span class="slider round"></span>
+     </label>
+      </div>
+    </div>
+  )
+}
+
+export default ShortCutList;
+
+
