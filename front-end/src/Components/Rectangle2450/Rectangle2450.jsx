031a89eca0954e54996fe015b54e734cd4243798
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:36:56 -0500

    Create Rectangle2450.jsx

---
+++ front-end/src/Components/Rectangle2450/Rectangle2450.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RectangleImage from '../../assets/Rectangle2450.svg';
+
+function Rectangle2450() {
+  return (
+    <img src={RectangleImage} className="rectangle2450"/>
+  )
+}
+
+export default Rectangle2450;
