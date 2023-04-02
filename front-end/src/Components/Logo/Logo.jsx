87d43863f7877a70656e3a1a5e3a32129ff6710b
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:32:53 -0500

    Create Logo.jsx

---
+++ front-end/src/Components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/Group1.svg';
+
+function Logo() {
+  return (
+    <div className='logo'>
+    <img src={logo} />
+    </div>
+  )
+}
+
+export default Logo;
