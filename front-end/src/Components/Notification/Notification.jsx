9836745472848dfa9afb4705aff390a347e1a288
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:34:25 -0500

    Create Notification.jsx

---
+++ front-end/src/Components/Notification/Notification.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import notification from "../../assets/notification.svg";
+
+function Notification() {
+  return (
+    <div className='group6563'>
+    <div className='frame1'>
+    <div className='five'>
+    5
+    </div>
+    </div>
+    <div className='notification'>
+    <img src={notification} />
+    </div>
+    </div>
+  )
+}
+
+export default Notification;
