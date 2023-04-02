bca18d7a9fd9fd44a8cf22d1462657f360bf3fe1
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:28:57 -0500

    Create CreateNewTour.jsx

---
+++ front-end/src/Components/CreateNewTour/CreateNewTour.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import add from "../../assets/add.svg";
+
+function CreateNewTour() {
+  return (
+    <div className='group7022'>
+    <div className='group7023'>
+    <div className='createNewTour'>
+    Create New Tour
+    </div>
+    <img src={add} className="add"/>
+    </div>
+    </div>
+  )
+}
+
+export default CreateNewTour;
