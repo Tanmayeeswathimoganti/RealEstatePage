41801ffce981a32f9c97b014d29debc0db0e0a8d
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:39:19 -0500

    Create SettingIcon.jsx

---
+++ front-end/src/Components/SettingIcon/SettingIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import settingIcon from '../../assets/setting2.svg';
+
+function SettingIcon() {
+  return (
+    <div className='setting'>
+    <img src={settingIcon} />
+    </div>
+  )
+}
+
+export default SettingIcon;
