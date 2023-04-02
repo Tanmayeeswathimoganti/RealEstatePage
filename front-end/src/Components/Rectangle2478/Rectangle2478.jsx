fd571f5244fed14cfc267986a012298e4c6f1277
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:37:21 -0500

    Create Rectangle2478.jsx

---
+++ front-end/src/Components/Rectangle2478/Rectangle2478.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RectangleImage from '../../assets/Rectangle2478.svg';
+
+function Rectangle2478() {
+  return (
+    <img src={RectangleImage} className="rectangle2478"/>
+  )
+}
+
+export default Rectangle2478;
