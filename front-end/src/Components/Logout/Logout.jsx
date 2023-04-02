2d931e4f7f3d9ef853e5a60fe2b2814e4d973222
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:33:16 -0500

    Create Logout.jsx

---
+++ front-end/src/Components/Logout/Logout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logout from '../../assets/logout.svg';
+
+function Logout() {
+  return (
+    <div className='logout'>
+    <img src={logout} />
+    </div>
+  )
+}
+
+export default Logout;
