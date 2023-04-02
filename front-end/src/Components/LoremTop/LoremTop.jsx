eeff68bf7476337b4ef7e5df1d9f0896c8f254d3
Tanmayeeswathimoganti <83610329+Tanmayeeswathimoganti@users.noreply.github.com>
2023-04-01 19:33:40 -0500

    Create LoremTop.jsx

---
+++ front-end/src/Components/LoremTop/LoremTop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoremTop() {
+  return (
+    <div className='loremTop'>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo risus a mattis semper. Integer placerat volutpat odio, at pulvinar tortor laoreet mattis. Proin aliquet semper tempus.
+    </div>
+  )
+}
+
+export default LoremTop;
